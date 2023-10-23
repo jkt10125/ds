dc5da8a6108ed9d357c2d92c6d8e79a456867a0d
jkt10125 <jkt10125@gmail.com>
2023-10-23 23:31:32 +0530

    improved indentation

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -1,59 +1,37 @@
 #include <bits/stdc++.h>
 
 std::vector<int> LIS(std::vector<int> &A) {
-
     int n = A.size();
     std::vector<int> lis(n), temp;
-
     for (int i = 0; i < n; i++) {
-
         int idx = std::lower_bound(temp.begin(), temp.end(), A[i]) - temp.begin();
-
         if (idx == temp.size()) {
-
             temp.push_back(A[i]);
         }
-
         else {
-
             temp[idx] = A[i];
         }
-
         lis[i] = idx + 1;
     }
-
     return lis;
 }
 
 int main() {
-
     int t;
-
     std::cin >> t;
-
     while (t--) {
-
         int n;
         std::cin >> n;
-
         std::vector<int> A(n);
-
         for (int i = 0; i < n; i++) {
-
             std::cin >> A[i];
         }
-
         auto lis = LIS(A);
-
         int ctr = 0;
-
         for (int i : lis) {
-
             ctr += (i == 2);
         }
-
         std::cout << ctr << "\n";
     }
-
     return 0;
 }
